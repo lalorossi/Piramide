38962d82bc7fdf772e60981e539c69fd183efbae
lalorossi <lalorossi@hotmail.com>
2015-06-27 20:38:32 -0300

    agrego comentarios

--- Piramide.py
+++ Piramide.py
@@ -1,17 +1,23 @@
+#La clase Juego es la base del programa. A partir de ella se comienza a resolver la piramide
 class Juego:
+
+	#Al iniciar el juego, se crea una piramide nueva
 	def __init__(self):
 		self._Tablero=Tablero()
 
 	def IniciarJuego(self):
 		pass
 
-
+#La clase Tablero Es solamente la piramide, tomada como un array de los numeros que contiene
 class Tablero:
+
+	#Al iniciar la Piramide, creamos todas sus celdas
+	#Se toma al 0 como celda vacia (Ya que no se puede ingresar un 0 en la piramide)
 	def __init__(self):
 		self._Piramide=[]
 		for filas in range(6):
 			print ("Ingrese los valores de izquierda a derecha de la fila %d" %(fila+1))
-			print ("O ingrese un espacio para dejarlo vacio")
+			print ("O ingrese un 0 para dejar la celda vacia")
 			for posicion in range(6-fila):
 				celda=Celda()
 				self._Piramide.append(celda)
@@ -24,8 +30,10 @@ class Tablero:
 			print self._Piramide[pos:numeros]
 			pos+=numeros
 
-
+#La clase celda son los elementos dentro del tablero, cada una con su valor
 class Celda:
+
+	#Al crear una celda, esta nos pregunta su valor
 	def __init__ (self):
 		Valor=input("Ingrese el valor de la celda")
 		if Valor == ' ':
@@ -34,4 +42,4 @@ class Celda:
 		self._Valor=Valor
 
 
-	NuevoJuego=Juego()
+NuevoJuego=Juego()
