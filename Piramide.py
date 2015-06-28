f149b4f31159636fc628ebb310b7bc66ab2e36f3
lalorossi <lalorossi@hotmail.com>
2015-06-28 14:44:01 -0300

    Arreglado el ingreso y mostrar la pirámide

--- Piramide.py
+++ Piramide.py
@@ -3,7 +3,7 @@ class Juego:
 
 	#Al iniciar el juego, se crea una piramide nueva
 	def __init__(self):
-		self._Tablero=Tablero()
+		self.Tablero=Tablero()
 
 	def IniciarJuego(self):
 		pass
@@ -14,32 +14,50 @@ class Tablero:
 	#Al iniciar la Piramide, creamos todas sus celdas
 	#Se toma al 0 como celda vacia (Ya que no se puede ingresar un 0 en la piramide)
 	def __init__(self):
-		self._Piramide=[]
+		self.Piramide=[]
+		print ("")
 		for filas in range(6):
-			print ("Ingrese los valores de izquierda a derecha de la fila %d" %(fila+1))
+			print ("")
+			print ("Ingrese los valores de izquierda a derecha de la fila %d" %(filas+1))
 			print ("O ingrese un 0 para dejar la celda vacia")
-			for posicion in range(6-fila):
+			print ("")
+			for posicion in range(6-filas):
 				celda=Celda()
-				self._Piramide.append(celda)
+				self.Piramide.append(celda)
 
 	def MostrarPiramide(self):
+		self._ArrayDeValores=[]
 		pos=0
 		numeros=0
+
+		#Agrego los valores a un array para poder mostrarlos más fácilmente
+		for i in range(21):
+			self._ArrayDeValores.append(self.Piramide[i].getValor())
+
+		print ("")
+
+		#muestro los primeros 6 valores, luego los siguientes 5, etc.
 		for i in range(6):
 			numeros=pos+6-i
-			print self._Piramide[pos:numeros]
-			pos+=numeros
+			print (self._ArrayDeValores[pos:numeros])
+			pos+=(6-i)
+
 
 #La clase celda son los elementos dentro del tablero, cada una con su valor
 class Celda:
 
 	#Al crear una celda, esta nos pregunta su valor
 	def __init__ (self):
-		Valor=input("Ingrese el valor de la celda")
+		Valor=input("Ingrese el valor de la celda: ")
 		if Valor == ' ':
 			Valor=0
 
 		self._Valor=Valor
 
+	def getValor(self):
+		return self._Valor
+
 
 NuevoJuego=Juego()
+
+NuevoJuego.Tablero.MostrarPiramide()
